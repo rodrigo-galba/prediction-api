f5a6d34160d6a3d8bfea146ac5167a1b0087c6fa
Rodrigo Galba <rodrigogalba@gmail.com>
2022-07-17 21:38:08 -0300

    Add predict endpoint

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from pydantic import BaseModel
 from mangum import Mangum
 
@@ -13,10 +13,26 @@ app = FastAPI(title="MyAwesomeApp", root_path=openapi_prefix)
 # Mangum Handler, this is so important
 handler = Mangum(app)
 
+class PredictModel(BaseModel):
+    message: str
 
 @app.get("/")
 def index():
     return {"message": "Prediction API"}
+
+@app.post("/predict")
+async def predict(model: PredictModel):
+    prediction = predict_completions(model.message)
+    return { "message": prediction }
+
+def predict_completions(message: str):
+    # Load model file
+    # Load unique words
+    # Call predict function
+    # lib/predict.py
+    # TODO roda as tretas do KERAS
+    return message
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
